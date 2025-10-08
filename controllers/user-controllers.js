c06f6ab4413989d212e66f4393682122b561e3bf
saiganeshpalla <ganesh@stockholmitacademy.org>
2025-10-08 11:48:31 +0530

    code

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,6 +1,7 @@
 
 var user = require("../model/user")
 var byCrpt = require("bcrypt")
+var webToken = require("jsonwebtoken")
 // register controller
 var register = async(req,res)=>{
     try{
@@ -40,26 +41,38 @@ var register = async(req,res)=>{
 
 var login = async(req,res)=>{
     try{
+        // collect the user information
         var {userName,password} = req.body
         var userThere = await user.findOne({userName})
         if(!userThere){
-            return res.status(400).json({messae : "invalid username and passsowrd"})
+            res.status(400).json({message : "invalid user name or password"})
         }
 
         var isPassword = await byCrpt.compare(password,userThere.password)
+        console.log(isPassword);
         if(!isPassword){
-            return res.status(400).json({message : "invalid username and password"})
-
-  
+            res.status(400).json({message : "invalid user name or password"})
         }
+        // generate a json web token
 
-        
+        var ganearteToeken = webToken.sign({
+            userId : userThere._id,
+            userName : userThere.userName,
+            role : userThere.role
+
+
+
+        },process.env.JSON_WEB_TOKEN,{expiresIn : "10m"})
+        res.status(200).json({message : "login sucess",token : ganearteToeken,sucess : true})
+
 
 
 
     }catch(error){
         console.log("error",error);
     }
+
+
 }
 
 module.exports = {
